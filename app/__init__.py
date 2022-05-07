d5baa8454139c3dfdc2d214bdd6412ac8ab55b87
atienoobwanda <atienoobwanda@gmail.com>
2022-05-07 20:23:20 +0300

    Application initialization complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from config import DevConfig
 
-
+# Initializing app
 app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+
+from app import views
