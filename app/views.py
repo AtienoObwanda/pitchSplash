97733775e1914656bc00c7894cc2576f44606e9d
atienoobwanda <atienoobwanda@gmail.com>
2022-05-07 21:16:15 +0300

    styling pitch cards

--- app/views.py
+++ app/views.py
@@ -2,16 +2,40 @@ from flask import render_template
 from app import app
 
 
-Pitches = [
+pitches = [
     {
         'author': 'Atieno Obwanda',
-        'pitchContent': 'Here and now... Today and always',
+        'content': 'Here and now... Today and always',
         'date_posted': 'April 20, 2022'
     },
     {
        
         'author': 'Mishie Gee',
-        'pitchContent': 'As above, as Below',
+        'content': 'As above, as Below',
+        'date_posted': 'June 20, 2022'
+    },
+    {
+       
+        'author': 'Mishie Gee',
+        'content': 'As above, as Below',
+        'date_posted': 'June 20, 2022'
+    },
+    {
+       
+        'author': 'Mishie Gee',
+        'content': 'As above, as Below',
+        'date_posted': 'June 20, 2022'
+    },
+    {
+       
+        'author': 'Mishie Gee',
+        'content': 'As above, as Below',
+        'date_posted': 'June 20, 2022'
+    },
+    {
+       
+        'author': 'Mishie Gee',
+        'content': 'As above, as Below',
         'date_posted': 'June 20, 2022'
     }
 ]
@@ -33,4 +57,4 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html', Pitches=Pitches)
+    return render_template('index.html', pitches=pitches)
