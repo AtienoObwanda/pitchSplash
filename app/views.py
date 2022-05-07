aceef90dd48478718bd46577500ba766b3e71230
atienoobwanda <atienoobwanda@gmail.com>
2022-05-07 20:42:58 +0300

    adding dummy picth for layout

--- app/views.py
+++ app/views.py
@@ -1,6 +1,31 @@
 from flask import render_template
 from app import app
 
+
+Pitches = [
+    {
+        'author': 'Atieno Obwanda',
+        'pitchContent': 'Here and now... Today and always',
+        'date_posted': 'April 20, 2022'
+    },
+    {
+       
+        'author': 'Mishie Gee',
+        'pitchContent': 'As above, as Below',
+        'date_posted': 'June 20, 2022'
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
 # Views
 @app.route('/')
 def index():
@@ -8,4 +33,4 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    return render_template('index.html', Pitches=Pitches)
