01498a3920233ab14c37863bef59abaf0d198b87
atienoobwanda <atienoobwanda@gmail.com>
2022-05-08 10:15:14 +0300

    Adding migration for pitches

--- app/models.py
+++ app/models.py
@@ -10,14 +10,13 @@ def load_user(user_id):
 #User Model
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
 
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(15), unique=True, nullable=False)
     email=db.Column(db.String(120), unique=True, nullable=False)
     image=db.Column(db.String(120), nullable=False, default='default.jpg')
     password=db.Column(db.String(60),nullable=False)
-    pitches = db.relationship('Post', backref='author',lazy=True) #defining the one to many relationship btn pitch and author
+    pitches = db.relationship('Pitch', backref='author',lazy=True) #defining the one to many relationship btn pitch and author
 
 
     def __repr__(self):
@@ -27,8 +26,7 @@ class User(UserMixin, db.Model):
 # Pitch
 
 class Pitch (db.Model):
-    __tablename__ = 'pitches'
-    
+
     id=db.Column(db.Integer, primary_key=True)
     datePosted = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from  flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 
 
-from app.models import User
+from app.models import User, Pitch
 
 
 # App instance
@@ -16,7 +16,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app,db=db,User=User)
+    return dict(app=app,db=db,User=User, Pitch=Pitch)
 
 if __name__ == '__main__':
     manager.run()
