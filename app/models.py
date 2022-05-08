3d9644feb08fe00e3eda01db269fe301c5346821
atienoobwanda <atienoobwanda@gmail.com>
2022-05-08 06:28:07 +0300

    configuring models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
-from . import db
+from flask_login import UserMixin
+from . import db, login_manager
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    
+
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(15), unique=True, nullable=False)
     email=db.Column(db.String(120), unique=True, nullable=False)
