fc45eeb82b229b08015cd3269dfb189166bfde09
atienoobwanda <atienoobwanda@gmail.com>
2022-05-08 05:58:31 +0300

    Initializing migrate

--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
-from app import create_app,db
 from flask_script import Manager, Server
+from  flask_migrate import Migrate, MigrateCommand
+
+from app import create_app,db
+
 
 from app.models import User
 
@@ -8,7 +11,8 @@ from app.models import User
 app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server', Server)
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
