49835e2335a0ae53ce7af8a566652630b5839627
atienoobwanda <atienoobwanda@gmail.com>
2022-05-08 09:33:14 +0300

    adding update account

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
 from flask import abort,request,redirect, render_template, url_for,flash
 from flask_login import login_user,current_user, logout_user, login_required
+import secrets
+from PIL import Image
 
 from .. import db,bcrypt
-from .forms import RegistrationForm, LoginForm
+from .forms import RegistrationForm, LoginForm,UpdateForm
 from ..models import User
 
 from . import auth
@@ -45,3 +47,41 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
+
+
+def save_picture(form_picture): # saving image
+    random_hex = secrets.token_hex(8) # geneates new name for the picture
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile', picture_fn)
+    
+    #image resizing
+    output_size=(125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path) # resized image
+
+    return picture_fn
+
+@auth.route('/account',methods=['GET', 'POST'])
+@login_required #restricts the accoun page to only logged in users
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data) # seting picture 
+            current_user.image = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash(f'Account details for {form.username.data} successfully updated!', 'sucess')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username # Populate user username on to the form
+        form.email.data = current_user.email # Populate user email on to the form
+    image = url_for('static', filename='profile/' + current_user.image) # route for default profile picture
+     
+    return render_template("account.html", title='Account', image=image, form=form)
+
