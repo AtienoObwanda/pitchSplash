6198891b5864b451b044311fa8e44e38b19a6dc6
atienoobwanda <atienoobwanda@gmail.com>
2022-05-08 07:06:02 +0300

    adding the auth routes for login and logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,14 +2,10 @@ from flask import abort,request,redirect, render_template, url_for,flash
 from flask_login import login_user,current_user, logout_user, login_required
 
 from .. import db,bcrypt
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from ..models import User
 
 from . import auth
-
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
 
 
 @auth.route('/register', methods=['GET','POST'])
@@ -29,3 +25,23 @@ def register():
 
 
 
+@auth.route('/login',methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form=LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember = form.remember.data)
+            next_page = request.args.get('next') #  when user tries to acccess restricted page
+            return redirect(next_page) if next_page else redirect(url_for('main.index')) # redirects to requested page after loggin in if it exists... if none, redirects to home page
+        else:
+            flash('Login Failed. Kindly check your email and password then try again','danger')
+    return render_template("auth/login.html",form=form,title='Flask Blog-Login')
+
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
