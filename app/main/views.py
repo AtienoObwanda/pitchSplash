d8e82d2273d28ae07fe835e58899143432afd7dd
atienoobwanda <atienoobwanda@gmail.com>
2022-05-08 17:28:05 +0300

    adding comment form template

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import abort,request,redirect, render_template, url_for,flash
 from flask_login import login_required,current_user
 
-from .form import PitchForm
+from .forms import PitchForm, CommentForm
 from . import main
 from ..models import Pitch
 from .. import db
@@ -31,7 +31,7 @@ def pitch():
         return redirect(url_for('main.index'))
     return render_template("pitch.html", title='Pitch-Splash | New Pitch', form=form)
 
-@main.route('/react/<int:pitch_id>')
+@main.route('/pitch/<int:pitch_id>/react')
 @login_required
 def react(pitch_id):
     pitch = Pitch.query.get_or_404(pitch_id)
@@ -41,3 +41,21 @@ def react(pitch_id):
         current_user.pitches_liked.remove(pitch)
         db.session.commit()
         return '',200
+
+
+@main.route('/comment/<int:pitch_id>',methods = ['POST','GET'])
+@login_required
+def comment(pitch_id):
+    pitch=Pitch.query.get_or_404(pitch_id)
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        pitch_id = pitch_id
+        db.session.add(comment)
+        db.session.commit()
+
+        flash('Comment added successfully')
+        
+        return redirect(url_for('main.comment',pitch_id=pitch_id))
+
+    return render_template("comment.html",pitch=pitch, title='React to Pitch!', form = form)
