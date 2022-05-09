7534031f48989b8ab5be9cf122b299795980c75a
atienoobwanda <atienoobwanda@gmail.com>
2022-05-10 01:06:55 +0300

    updatin env

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY=625a182b00fc0162681e684661501fc4
 export DATABASE_URL=postgresql+psycopg2://atieno:mishi@localhost/pitch
 export MAIL_USERNAME=splashpitch@gmail.com
-export MAIL_PASSWORD=afSG7AyE123!!
+export MAIL_PASSWORD=afSG7AyE123clear
 
 python3.9 manage.py server
