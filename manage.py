b685fa1dad5f32916364fb7fd8b7999fca6a8cc6
atienoobwanda <atienoobwanda@gmail.com>
2022-05-09 10:47:22 +0300

    Initializing tests inside manage.py and running it | working as expected

--- manage.py
+++ manage.py
@@ -8,15 +8,25 @@ from app.models import User, Pitch
 
 
 # App instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
+
 manager.add_command('db', MigrateCommand)
+manager.add_command('run',Server(use_debugger=True))
+
 
 @manager.shell
 def make_shell_context():
     return dict(app=app,db=db,User=User, Pitch=Pitch)
+
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
